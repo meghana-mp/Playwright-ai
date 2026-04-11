c5d822d6ad41f9c3ea5e5cead4b72dc5513baea9
Meghana Punyavathi <meghana.mp.19@gmail.com>
2026-04-11 18:23:50 +0530

    commit first test after repo renamed

--- tests/apitests/FirstAPI.spec.ts
+++ tests/apitests/FirstAPI.spec.ts
@@ -1,5 +1,8 @@
 import { test, expect } from '@playwright/test';
 
+
+
+//First MockTest to validate if the local wiremock server is running on port 9090 and returning the expected response for the /api/success endpoint. Make sure to update the port in the request URL to match your WireMock configuration.
 test('validate local wiremock on port 9090', async ({ request }) => {
   // Update the port here to 9090
   const response = await request.get('http://127.0.0.1:9090/api/success');
